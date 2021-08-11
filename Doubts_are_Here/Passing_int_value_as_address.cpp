e0c066bca6bcfe3889eb97fce01d55f7976939ae
Ayush Kumar <hellotoayush@gmail.com>
2021-08-11 22:48:28 +0530

    Passing int value to function
    Resolved by Prajal Friend
    
    Solution :
        Bhai array hamsa pass by reference hota hai ...
        *Pointer lagane ki jrurat nhi

--- Doubts_are_Here/Passing_int_value_as_address.cpp
+++ Doubts_are_Here/Passing_int_value_as_address.cpp
@@ -1,28 +1,34 @@
-// Only two array are passing arr[0] and arr[1]
-// and gettion error when passing third array arr[2]
+/* 
+!Error.
+        Only two array are passing arr[0] and arr[1]
+        and gettion error when passing third array arr[2]
+
+Solution : 
+    Bhai array hamsa pass by reference hota hai ...
+    *Pointer lagane ki jrurat nhi
+
+*/
 
 #include<iostream>
 using namespace std;
 
-void pass_integer(int *arr[])
+void pass_integer(int *arr)
 {
-    *arr[0] = 545;
-    *arr[1] = 76;
+    arr[0] = 545;
+    arr[1] = 76;
 
-    
-    // *arr[2] = 87;   // unable to pass this third value
-
-
+    arr[2] = 87;   // Now able to pass this third value
+    arr[3] = 100;
 }
 
-void output(int *arr[])
+void output(int arr[])
 {
-    cout<< *arr[0] << endl;
-    cout<< *arr[1] << endl;    
-    
+    cout<< arr[0] << endl;
+    cout<< arr[1] << endl;        
 
-    // Just comment this below line then program will run easily else not
-    // cout<< *arr[2] << endl;    // this value is not printing
+    // Just comment this below line then program will run easily else now running
+    cout<< arr[2] << endl;    // this value is now printing
+    cout<< arr[3] << endl;
 }
 
 int main()
@@ -31,7 +37,7 @@ int main()
     int* arr;
     arr = new int[100];
 
-    pass_integer(&arr);
+    pass_integer(arr);
 
-    output(&arr);
+    output(arr);
 }
