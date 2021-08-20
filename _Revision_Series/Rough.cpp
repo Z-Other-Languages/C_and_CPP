9e86c3bfd2ea61168c5f18136ca44548f92c7dd8
Ayush Kumar <hellotoayush@gmail.com>
2021-08-20 13:38:43 +0530

    Create Rough.cpp

---
+++ _Revision_Series/Rough.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    arr[0] = 100;
+    // arr[1] = NULL;
+    // arr[16] = 45;
+    cout<< endl <<  arr[0] << endl;
+    // cout<< arr[16] << endl;
+
+    return 0;
+}
